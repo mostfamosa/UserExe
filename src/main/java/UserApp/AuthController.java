e6765b062bf49262246d297adb555bf9c68d256b
khaledwani27 <wanikhaled27@gmail.com>
2022-10-30 21:19:20 +0200

    singleton controllers and services

--- src/main/java/UserApp/AuthController.java
+++ src/main/java/UserApp/AuthController.java
@@ -1,4 +1,20 @@
 package UserApp;
 
 public class AuthController {
+
+    private static AuthController authController;
+
+    private final AuthService authService;
+    private AuthController()
+    {
+        authService=AuthService.getInstance();
+    }
+
+    public static AuthController getInstance()
+    {
+        if (authController == null){
+            authController = new AuthController();
+        }
+        return authController;
+    }
 }
--- src/main/java/UserApp/UserController.java
+++ src/main/java/UserApp/UserController.java
@@ -1,4 +1,20 @@
 package UserApp;
 
 public class UserController {
+    private static UserController userController;
+    private final AuthService authService;
+    private final UserService userService;
+    private UserController()
+    {
+        authService=AuthService.getInstance();
+        userService=UserService.getInstance();
+    }
+
+    public static UserController getInstance()
+    {
+        if (userController == null){
+            userController = new UserController();
+        }
+        return userController;
+    }
 }
