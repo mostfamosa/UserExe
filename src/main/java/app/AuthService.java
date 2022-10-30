7def95a68e8e07d0b8cbd01c93011fa95b15a530
MayaBarkann <mayabarkann@gmail.com>
2022-10-30 20:59:28 +0200

    started working on authservice + added user class

--- src/main/java/app/AuthService.java
+++ src/main/java/app/AuthService.java
@@ -2,8 +2,20 @@ package app;
 
 public class AuthService {
 
-
-    protected User createNewUser(){
+    // TODO
+    protected User createNewUser(String email, String name, String password){
+        if (emailExists(email)){
+            throw new IllegalArgumentException("This email already exists");
+        }
         return null;
+        // write validation method to validate that the users repo does not contain a user with this email.
+
     }
+
+    //TODO
+    private static boolean emailExists(String email) {
+        return false;
+    }
+
+
 }
